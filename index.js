e1f8fd3642aacc17896d86a274260f65cf1b6524
Alexander Hendrawan tjahja & YONI SETIAWAN <pair+alexanderHT.yonisetiawan@devbootcamp.com>
2016-10-07 14:32:22 +0700

    selesai

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-index.js
+"use strict"
+// kamus
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+// kamus
+
+// data
+var database = fs.readFileSync('data.json')
+var data = JSON.parse(database)
+// data
+
+// welcome
+console.log("Welcome to JS Flash Cards. To play, just enter the correct term for each definition. Ready? Go!");
+// Welcome
+
+// create inteface (pousedoucode)
+// interface.setPrompt(pertanyaan)
+// interface.prompt()
+// var i =0
+//
+// if (i < data.length) {
+//   if (jawaban benar) {
+//     interface.setPrompt(next_pertanyaan)
+//     interface.prompt()
+//     i++
+//   } else {
+//     interface.prompt()
+//   }
+//
+// }
+
+// setting
+var i = 0
+
+// proses reqursiv
+function permainan(answer){
+  if(i == ( data.length )){
+    rl.close();
+  }else{
+
+    rl.question(data[i].pertanyaan + " : ", (answer) => {
+      if(data[i].answer == answer){
+        console.log("\n selamat anda benar!");
+        i++
+        permainan(answer)
+      }else{
+        console.log("jawaban anda salah! tebak lagi");
+        permainan(answer)
+      }
+
+    });
+  }
+}
+
+permainan()
